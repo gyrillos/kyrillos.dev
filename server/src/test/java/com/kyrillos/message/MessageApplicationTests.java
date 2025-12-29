d559eadb429306f46992ac6e97e3796e38037aab
Kyrillos Abdelshaheed <kyrillos.abdelshaheed.22@cnu.edu>
2025-12-29 15:19:15 -0500

    Inital project commit

---
+++ server/src/test/java/com/kyrillos/message/MessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyrillos.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
