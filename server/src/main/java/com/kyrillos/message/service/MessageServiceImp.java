63562e2613b4d2260c03b2d13c3f961a0ccb7e12
Kyrillos Abdelshaheed <kyrillos.abdelshaheed.22@cnu.edu>
2026-05-27 08:51:51 -0400

    finished implementation of email feature, concludes backend dev

--- server/src/main/java/com/kyrillos/message/service/MessageServiceImp.java
+++ server/src/main/java/com/kyrillos/message/service/MessageServiceImp.java
@@ -1,10 +1,13 @@
 package com.kyrillos.message.service;
 
 import java.util.List;
+import java.util.Properties;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
+import jakarta.mail.*;
+import jakarta.mail.internet.*;
 import com.kyrillos.message.dao.MessageDAO;
 import com.kyrillos.message.entity.Message;
 
@@ -14,6 +17,15 @@ import jakarta.transaction.Transactional;
 public class MessageServiceImp implements MessageService{
 	
 	private MessageDAO messageDAO;
+	
+	@Value("${app.email.username}")
+    private String fromEmail;
+
+    @Value("${app.email.password}")
+    private String appPassword;
+
+    @Value("${app.email.to}")
+    private String toEmail;
 	
 	@Autowired
 	public MessageServiceImp(MessageDAO messageDAO) {
@@ -28,7 +40,59 @@ public class MessageServiceImp implements MessageService{
 	@Override
 	@Transactional
 	public Message save(Message m) {
-		
+		Properties props = new Properties();
+
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(fromEmail, appPassword);
+            }
+        });
+
+        try {
+        	 try {
+             	
+                 jakarta.mail.Message message = new MimeMessage(session);
+
+                 message.setFrom(new InternetAddress(fromEmail));
+                 message.setRecipients(
+                         jakarta.mail.Message.RecipientType.TO,
+                         InternetAddress.parse(m.getEmail())
+                 );
+
+                 message.setSubject("Response From Kyrillos.dev");
+                 message.setText("Hello, \n\n Thank you from your message! I'll try to get back to you as soon as possible.\n\n --\r\n"
+                 		+ "Software Engineer\r\n"
+                 		+ "Bachelor of Science in Computer Science\r\n"
+                 		+ "tel: 757-375-8368\r\n"
+                 		+ "linkedin.com/in/kyrillos-abdelshaheed");
+
+                 Transport.send(message);
+
+             } catch (MessagingException e) {
+                 throw new RuntimeException("Failed to send email, User Email incorrect", e);
+             }
+            jakarta.mail.Message message = new MimeMessage(session);
+
+            message.setFrom(new InternetAddress(fromEmail));
+            message.setRecipients(
+                    jakarta.mail.Message.RecipientType.TO,
+                    InternetAddress.parse(toEmail)
+            );
+
+            message.setSubject("New Message on Kyrillos.dev!!!");
+            message.setText("New Message from Sender: " + m.getEmail() + "\n\n" + m.getMessage());
+
+            Transport.send(message);
+
+        } catch (MessagingException e) {
+            throw new RuntimeException("Failed to send email", e);
+        }
 		return messageDAO.save(m);
 	}
 
