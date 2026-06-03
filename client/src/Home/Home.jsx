51344854e3ebd4e3be22180dae95b4d752b95aa0
Kyrillos Abdelshaheed <kyrillos.abdelshaheed.22@cnu.edu>
2026-06-03 06:48:12 -0400

    addded files

---
+++ client/src/Home/Home.jsx
@@ -0,0 +1,12 @@
+import "./Home.css"
+import Navbar from "../Navbar/Navbar";
+
+function Home() {
+    return (
+        <div className="home">
+            <Navbar/>
+        </div>
+    );
+}
+
+export default Home;
---
+++ client/src/Projects/Projects.jsx
@@ -0,0 +1,11 @@
+import "./Projects.css"
+
+function Projects() {
+    return (
+        <div className="Projects">
+
+        </div>
+    );
+}
+
+export default Projects;
