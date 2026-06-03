86114d5ffd5c84d7a6c54f6ff6de025db99d4222
Kyrillos Abdelshaheed <kyrillos.abdelshaheed.22@cnu.edu>
2026-06-03 07:45:18 -0400

    home file structure welcome featured and contact

--- client/src/Home/Home.jsx
+++ client/src/Home/Home.jsx
@@ -1,10 +1,16 @@
 import "./Home.css"
 import Navbar from "../Navbar/Navbar";
+import Welcome from "../Welcome/Welcome";
+import Featured from "../Featured/Featured";
+import Contact from "../Contact/Contact";
 
 function Home() {
     return (
         <div className="home">
-            <Navbar/>
+            <Navbar></Navbar>
+            <Welcome></Welcome>
+            <Featured></Featured>
+            <Contact></Contact>
         </div>
     );
 }
