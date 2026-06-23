984bc06a8cf24d70f6eede39331e40d81d967d70
Kyrillos Abdelshaheed <kyrillos.abdelshaheed.22@cnu.edu>
2026-06-23 16:23:07 -0400

    fixed vite config for localhost call

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,15 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:8080',
-    },
-  },
   preview: {
       allowedHosts: [
         "kyrillos.dev",
         "www.kyrillos.dev"
       ]
     }
-})
+});
